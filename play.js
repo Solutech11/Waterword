b98c27c69f649fe44110734772e8946f6a1538a3
OmoniyiIfeAbraham <86631380+OmoniyiIfeAbraham@users.noreply.github.com>
2021-08-05 13:03:49 +0100

    Add files via upload

--- play.js
+++ play.js
@@ -1,18 +1,16 @@
             //javascript
-            // console.log(localStorage.getItem("waterLevel"));
-            // if(localStorage.getItem("waterLevel")==="" or )
-            //Variables
-            //Button
-            var Yes = document.querySelector(".YesPlay"),
-                No = document.querySelector(".NoExit")
+    //Variables
+//Button
+var Yes = document.querySelector(".YesPlay"),
+    No =document.querySelector(".NoExit")
 
 
-            Pagelinking(Yes, "scene.html")
-            Pagelinking(No, "exit.html")
-                // levelButton(level1, "Game1.html");
-                // Yes.addEventListener("click",function(){
-                //     window.close()
-                // })
+Pagelinking(Yes, "scene.html")
+Pagelinking(No, "exit.html")
+// levelButton(level1, "Game1.html");
+// Yes.addEventListener("click",function(){
+//     window.close()
+// })
 
 
 
@@ -24,15 +22,9 @@
 
 
 
-            // functions are here 
-            function Pagelinking(buttons, theLevelLink) {
-                buttons.addEventListener("click", function() {
-                    window.location.href = theLevelLink
-                })
-            }
-
-
-            function plat() {
-                var mut = new Audio('Yanni_Ethnicity.mp3');
-                mut.play();
-            }
+// functions are here 
+function Pagelinking(buttons, theLevelLink){
+    buttons.addEventListener("click", function(){
+        window.location.href= theLevelLink
+    })
+}
